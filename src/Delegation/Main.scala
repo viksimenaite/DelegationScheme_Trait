fba1a40774ee31d9af8aafa8a606c2000ce5b1da
viksimenaite <55063269+viksimenaite@users.noreply.github.com>
2020-11-11 19:37:21 +0200

    Change entities and add functionality

--- src/Delegation/Main.scala
+++ src/Delegation/Main.scala
@@ -1,5 +1,7 @@
 package Delegation
 
+import java.time.LocalDate
+
 object Main {
   def main(args: Array[String]): Unit = {
 
@@ -7,59 +9,110 @@ object Main {
 
 }
 
+sealed trait Accessories
+case class Sprinkles() extends Accessories
+case class Berries() extends Accessories
+case class ChocolateChips() extends Accessories
+
+sealed trait EatingHabits
+case class Vegan() extends EatingHabits
+case class Vegetarian() extends EatingHabits
+case class EatsEverything() extends EatingHabits
+
 //entities
 
-case class Shoe(isLeather:Boolean, isLimitedEdition:Boolean, basePrice:Double, isLuxury:Boolean)
+case class FoodProduct(intendedFor: EatingHabits, expiryDate:String, baseKgPrice:Double, containsLactose:Boolean)
 
-class ShoeFactory{ //1st entity
-  def makeShoes(isFullWorkDay:Boolean):Int = if(isFullWorkDay){
-    2500
-  }else{
-    1000
-  }
+class CakeFactory{
 
-  def sellAtBasePrice(shoe:Shoe): Double = shoe.basePrice
-
-}
-
-class ShoeWarehouse{ //2nd entity
-  val maxNoOfShoesPairs = 500000
-  var currentNoOfShoesPairs = 0
-  val storageFeePerPair = 0.05
-  def storeShoes(noOfPairs:Int):Boolean={
-    if((currentNoOfShoesPairs + noOfPairs) <= maxNoOfShoesPairs){
-      currentNoOfShoesPairs = currentNoOfShoesPairs + noOfPairs
-      true //successfully stored
+  def countPrice(product:FoodProduct): Double = {
+    if (product.intendedFor == Vegan()){
+      product.baseKgPrice + 8
+    }else if(product.intendedFor == Vegetarian()){
+      product.baseKgPrice + 6
+    }else if(!product.containsLactose){
+      product.baseKgPrice + 4
     }else{
-      false
+      product.baseKgPrice + 2
     }
+
   }
 
-  def requestShoes(noOfPairs:Int): Double = noOfPairs * storageFeePerPair;
 }
 
-class ShoeShop{ //3rd entity
-  var currentNoOfShoesPairs = 0
+class ChocolateFactory{
+  def countPricePer200g(product:FoodProduct): Double = product.baseKgPrice * 0.2
+}
 
-  def sellShoes(isLuxury:Boolean):Double= if(isLuxury){
-    190.00
-  }else{
-    50.90
+class CupcakeFactory{
+  val averageWeightInGrams = 100
+  def countPricePerOne(product:FoodProduct): Double =  if(product.containsLactose){
+    product.baseKgPrice * averageWeightInGrams/1000 + 1
+  } else{
+    product.baseKgPrice * averageWeightInGrams/1000 + 2
   }
 
-  def reserveShoes(isPopular:Boolean):Double = if(isPopular){
-    7.00 //reservation fee
-  }else{
-    3.5 //reservation fee
-  }
-
-  def receiveShoes(noOfPairs:Int):Unit = {
-    currentNoOfShoesPairs = currentNoOfShoesPairs + noOfPairs
+  def sellRequiredAmount(product:FoodProduct, quantity:Int): Double ={
+    if (quantity>10){
+      countPricePerOne(product) * quantity * 0.9
+    }else {
+      countPricePerOne(product) * quantity
+    }
   }
 }
 
 //additional functionalities
-trait shoeDecorating{
 
+trait Decorating{
+  def addSprinkles(product:FoodProduct):Double
+  def addMarshmallows(product:FoodProduct):Double
 }
+
+trait Discounts{
+  def countExpirationDiscount(product:FoodProduct):Double
+}
+
+trait Suitability{
+  def isSuitableForEverybody(product:FoodProduct): Boolean
+  def isSuitableForNonMeatEaters(product:FoodProduct): Boolean
+}
+
+//additional functionalities' implementations
+
+trait DecoratingWithToppings extends Decorating{
+  def addSprinkles(product:FoodProduct):Double = 0.5 //add to price
+  def addMarshmallows(product:FoodProduct):Double = if(product.intendedFor == Vegan()){
+    0
+  }else{
+    1.2
+  }
+}
+
+trait HalfPriceDiscount{
+  def countExpirationDiscount(product:FoodProduct):Double = {
+    val dtf = java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    val expiryDate = java.time.LocalDate.parse(product.expiryDate, dtf)
+
+    if(LocalDate.now.isEqual(expiryDate) || LocalDate.now.isAfter(expiryDate)){
+      0.5
+    }else{
+      0
+    }
+  }
+
+  trait SuitabilityForDifferentEatingHabits{
+    def isSuitableForEverybody(product:FoodProduct): Boolean = if(product.intendedFor == Vegan() && !product.containsLactose){
+      true
+    }else{
+      false
+    }
+    def isSuitableForNonMeatEaters(product:FoodProduct): Boolean = if(product.intendedFor == Vegan() || product.intendedFor == Vegetarian()){
+      true
+    }else{
+      false
+    }
+  }
+}
+
+
 
